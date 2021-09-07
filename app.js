8510d9901a548536e826ad413695fb01a6bd1735
= <=>
2021-09-07 18:57:03 +0530

    Routes completed

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const port = 5000
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
+
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')        - get all the tasks
+// app.post('/api/v1/tasks')       - create a new task
+// app.get('/api/v1/tasks/:id')    - get single task
+// app.patch('/api/v1/tasks/:id')  - update task
+// app.delete('/api/v1/tasks/:id') - delete task
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
