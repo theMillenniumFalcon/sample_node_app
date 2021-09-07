4b19371364dee8f5f90c0122b9ef7a90eb0400ee
= <=>
2021-09-08 01:24:28 +0530

    API testing completed

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
 const tasks = require('./routes/tasks')
 
 // middleware
 app.use(express.json())
-
 
 // routes
 app.get('/hello', (req, res) => {
